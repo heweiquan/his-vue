a20633d5bd7853ad15587412fe6245d39845702c
heweiquan <hwq@lenovo.com>
2025-10-09 22:50:29 +0800

    3-8

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,14 +1,30 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router';
 const history = createWebHistory();
-const routes: Array<RouteRecordRaw> = [
+const routes : Array<RouteRecordRaw> = [
     {
-        path: '/front/index',
-        name: '/front/index',
-        component: () => import('../views/front/index.vue')
+        path: '/front',
+        name: 'Front',
+        component: () => import('../views/front/main.vue'),
+        children: [
+            {
+                path: 'index',
+                name: 'FrontIndex',
+                component: () => import('../views/front/index.vue')
+            }
+        ]
+    },
+
+    {
+        path: '/mis',
+        name: 'Main',
+        component: () => import('../views/mis/main.vue'),
+        children: [
+            
+        ]
     },
     {
         path: '/mis/login',
-        name: '/mis/login',
+        name: 'MisLogin',
         component: () => import('../views/mis/login.vue')
     },
     {
@@ -17,7 +33,7 @@ const routes: Array<RouteRecordRaw> = [
         component: () => import('../views/404.vue')
     },
     {
-        path: '/:pathMath(.*)*',
+        path: '/:pathMatch(.*)*',
         redirect: '/404'
     }
 ];
@@ -25,4 +41,27 @@ const router = createRouter({
     history,
     routes
 });
+
+router.beforeEach((to, from, next) => {
+    let permissions = localStorage.getItem('permissions');
+    let token = localStorage.getItem('token');
+    let fullPath = to.fullPath;
+    if (fullPath.startsWith('/mis') && fullPath != '/mis/login') {
+        if (permissions == null || permissions == '' || token == null || token == '') {
+            next({ name: 'MisLogin' });
+        } else {
+            return next();
+        }
+    } else if (fullPath.startsWith('/front/customer') || fullPath.startsWith('/front/goods_snapshot')) {
+        if (token == null || token == '') {
+            next({ name: 'FrontIndex' });
+        } else {
+            return next();
+        }
+    } else {
+        return next();
+    }
+});
+
 export default router;
+
