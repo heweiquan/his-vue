0fdbacb8dbbddc8a8c06f38f9b545a373c0dd8d7
heweiquan <hwq@lenovo.com>
2025-10-09 23:11:52 +0800

    3-10

--- components.d.ts
+++ components.d.ts
@@ -8,6 +8,8 @@ export {}
 declare module '@vue/runtime-core' {
   export interface GlobalComponents {
     ElButton: typeof import('element-plus/es')['ElButton']
+    ElInput: typeof import('element-plus/es')['ElInput']
+    ElTag: typeof import('element-plus/es')['ElTag']
     RouterLink: typeof import('vue-router')['RouterLink']
     RouterView: typeof import('vue-router')['RouterView']
     SvgIcon: typeof import('./src/components/SvgIcon.vue')['default']
