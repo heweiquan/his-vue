1db1f707d26447d42927e969f2eb62950c724465
heweiquan <hwq@lenovo.com>
2025-10-10 09:06:32 +0800

    3-14

--- src/views/mis/main.vue
+++ src/views/mis/main.vue
@@ -30,6 +30,152 @@
         </div>
       </div>
     </nav>
+    <aside class="site-sidebar site-sidebar--dark">
+      <div class="site-sidebar__inner">
+        <el-menu :default-active="siteContent.menuActiveName || 'Home'" :collapse="sidebar.sidebarFold"
+          :collapseTransition="false" class="site-sidebar__menu" background-color="#263238" active-text-color="#fff"
+          text-color="#8a979e">
+          <el-menu-item index="Home" @click="$router.push({ name: 'MisHome' })">
+            <el-icon>
+              <SvgIcon name="home" class="icon-svg" />
+            </el-icon>
+            <span slot="title">首页</span>
+          </el-menu-item>
+          <el-sub-menu index="组织管理" :popper-class="'site-sidebar--' + sidebar.sidebarLayoutSkin + '-popper'">
+            <template #title>
+              <el-icon>
+                <SvgIcon name="users_fill" class="icon-svg" />
+              </el-icon>
+              <span slot="title">组织管理</span>
+            </template>
+            <el-menu-item index="MisDept" v-if="proxy.isAuth(['ROOT', 'DEPT:SELECT'])"
+              @click="$router.push({ name: 'MisDept' })">
+              <el-icon>
+                <SvgIcon name="company_fill" class="icon-svg" />
+              </el-icon>
+              <span slot="title">部门管理</span>
+            </el-menu-item>
+            <el-menu-item index="MisRole" v-if="proxy.isAuth(['ROOT', 'ROLE:SELECT'])"
+              @click="$router.push({ name: 'MisRole' })">
+              <el-icon>
+                <SvgIcon name="role_fill" class="icon-svg" />
+              </el-icon>
+              <span slot="title">角色管理</span>
+            </el-menu-item>
+            <el-menu-item index="MisUser" v-if="proxy.isAuth(['ROOT', 'USER:SELECT'])"
+              @click="$router.push({ name: 'MisUser' })">
+              <el-icon>
+                <SvgIcon name="user_fill" class="icon-svg" />
+              </el-icon>
+              <span slot="title">用户管理</span>
+            </el-menu-item>
+          </el-sub-menu>
+          <el-sub-menu index="业务管理" :popper-class="'site-sidebar--' + sidebar.sidebarLayoutSkin + '-popper'">
+            <template #title>
+              <el-icon>
+                <SvgIcon name="trust_fill" class="icon-svg" />
+              </el-icon>
+              <span slot="title">业务管理</span>
+            </template>
+            <el-menu-item index="MisGoods" v-if="proxy.isAuth(['ROOT', 'GOODS:SELECT'])"
+              @click="$router.push({ name: 'MisGoods' })">
+              <el-icon>
+                <SvgIcon name="goods_fill" class="icon-svg" />
+              </el-icon>
+              <span slot="title">体检套餐</span>
+            </el-menu-item>
+            <el-menu-item index="MisRule" v-if="proxy.isAuth(['ROOT', 'RULE:SELECT'])"
+              @click="$router.push({ name: 'MisRule' })">
+              <el-icon>
+                <SvgIcon name="rule_fill" class="icon-svg" />
+              </el-icon>
+              <span slot="title">促销规则</span>
+            </el-menu-item>
+            <el-menu-item index="MisCustomer" v-if="proxy.isAuth(['ROOT', 'CUSTOMER:SELECT'])"
+              @click="$router.push({ name: 'MisCustomer' })">
+              <el-icon>
+                <SvgIcon name="customer_fill" class="icon-svg" />
+              </el-icon>
+              <span slot="title">客户档案</span>
+            </el-menu-item>
+            <el-menu-item index="MisOrder" v-if="proxy.isAuth(['ROOT', 'ORDER:SELECT'])"
+              @click="$router.push({ name: 'MisOrder' })">
+              <el-icon>
+                <SvgIcon name="order_fill" class="icon-svg" />
+              </el-icon>
+              <span slot="title">订单管理</span>
+            </el-menu-item>
+            <el-menu-item index="MisCustomerIm" v-if="proxy.isAuth(['ROOT', 'CUSTOMER_IM:SELECT'])"
+              @click="$router.push({ name: 'MisCustomerIm' })">
+              <el-icon>
+                <SvgIcon name="im_fill" class="icon-svg" />
+              </el-icon>
+              <span slot="title">客服IM</span>
+            </el-menu-item>
+          </el-sub-menu>
+          <el-sub-menu index="体检管理" :popper-class="'site-sidebar--' + sidebar.sidebarLayoutSkin + '-popper'">
+            <template #title>
+              <el-icon>
+                <SvgIcon name="night_fill" class="icon-svg" />
+              </el-icon>
+              <span slot="title">体检管理</span>
+            </template>
+            <el-menu-item index="MisAppointment" v-if="proxy.isAuth(['ROOT', 'APPOINTMENT:SELECT'])"
+              @click="$router.push({ name: 'MisAppointment' })">
+              <el-icon>
+                <SvgIcon name="appointment_fill" class="icon-svg" />
+              </el-icon>
+              <span slot="title">体检预约</span>
+            </el-menu-item>
+            <el-menu-item index="MisCustomerCheckin" v-if="proxy.isAuth(['ROOT', 'CUSTOMER_CHICKIN:SELECT'])"
+              @click="$router.push({ name: 'MisCustomerCheckin' })">
+              <el-icon>
+                <SvgIcon name="checkin_fill" class="icon-svg" />
+              </el-icon>
+              <span slot="title">体检签到</span>
+            </el-menu-item>
+            <el-menu-item index="MisAppointmentRestriction"
+              v-if="proxy.isAuth(['ROOT', 'APPOINTMENT_RESTRICTION:SELECT'])"
+              @click="$router.push({ name: 'MisAppointmentRestriction' })">
+              <el-icon>
+                <SvgIcon name="setting_fill" class="icon-svg" />
+              </el-icon>
+              <span slot="title">预约设置</span>
+            </el-menu-item>
+            <el-menu-item index="MisCheckup" v-if="proxy.isAuth(['ROOT', 'CHECKUP:SELECT'])"
+              @click="$router.push({ name: 'MisCheckup' })">
+              <el-icon>
+                <SvgIcon name="doctor_fill" class="icon-svg" />
+              </el-icon>
+              <span slot="title">医生检查</span>
+            </el-menu-item>
+            <el-menu-item index="MisCheckupReport" v-if="proxy.isAuth(['ROOT', 'CHECKUP_REPORT:SELECT'])"
+              @click="$router.push({ name: 'MisCheckupReport' })">
+              <el-icon>
+                <SvgIcon name="file_fill" class="icon-svg" />
+              </el-icon>
+              <span slot="title">体检报告</span>
+            </el-menu-item>
+          </el-sub-menu>
+          <el-sub-menu index="系统设置" :popper-class="'site-sidebar--' + sidebar.sidebarLayoutSkin + '-popper'"
+            v-if="proxy.isAuth(['ROOT', 'SYSTEM:SELECT'])">
+            <template #title>
+              <el-icon>
+                <SvgIcon name="system_fill" class="icon-svg" />
+              </el-icon>
+              <span slot="title">系统设置</span>
+            </template>
+            <el-menu-item index="MisFlowRegulation" v-if="proxy.isAuth(['ROOT', 'FLOW_REGULATION:SELECT'])"
+              @click="$router.push({ name: 'MisFlowRegulation' })">
+              <el-icon>
+                <SvgIcon name="people_fill" class="icon-svg" />
+              </el-icon>
+              <span slot="title">人员限流</span>
+            </el-menu-item>
+          </el-sub-menu>
+        </el-menu>
+      </div>
+    </aside>
     <!-- 避免路由引用页面的时候浏览器不刷新内容，所以给URL添加随机数参数 -->
     <router-view :key="router.currentRoute.value.query.random" />
   </div>
